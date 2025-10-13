c4d9757f6156cd83e2536d745669375443b6d338
Harry <hariharasethi89@gmail.com>
2025-10-13 23:01:21 +0530

    fix: integrate validation

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,14 +1,13 @@
 import { ZodType } from "zod";
 import type { Request, Response, NextFunction } from "express";
+import { BadRequestError } from "@/errors/ApiError";
 
 export function validate<Type>(schema: ZodType<Type>) {
-  return function (req: Request, res: Response, next: NextFunction) {
+  return function (req: Request, _: Response, next: NextFunction) {
     const result = schema.safeParse(req.body);
 
     if (!result.success) {
-      res.status(400).json({
-        message: result.error.message,
-      });
+      throw new BadRequestError(result.error.message)
     }
 
     req.body = result.data;
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { authController } from "@/controllers/auth.controller.js";
-import { verifyToken } from "@/middlewares/auth.middleware";
+import { verifyToken } from "@/middlewares/auth.middleware.js";
+import { validate } from "@/middlewares/validation.middleware.js";
+import { registerSchema, loginSchema } from "@/validations/auth.schema.js"
 
 const router = Router();
 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+router.post("/register", validate(registerSchema), authController.register);
+router.post("/login", validate(loginSchema), authController.login);
 router.get("/logout", verifyToken, authController.logout);
 router.get("/refresh", authController.refresh);
 
